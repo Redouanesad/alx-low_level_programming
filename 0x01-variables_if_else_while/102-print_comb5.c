62c9668ff4d42ac6ca26f7d19aaab28a177c93b1
Redouanesad <redouanesadouk7@gmail.com>
2023-06-16 18:33:02 -0700

    a

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,7 +4,7 @@
  * main - Entry point
  *
  * Description: write a program that prints all possible
- * different combinations of two digits
+ *		different combinations of two digits
  *
  * Return: Always 0 (Success)
 */
