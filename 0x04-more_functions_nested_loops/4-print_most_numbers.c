7c31bbd47707e27244fd3bf2c0a826c6a58e1f63
Redouanesad <redouanesadouk7@gmail.com>
2023-06-22 11:01:50 -0700

    a

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,10 +11,10 @@ void print_most_numbers(void)
 {
 	int num;
 
-	for (num = 1; num <= 10; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		if (num == 2 || num == 4)
-			contine;
+			continue;
 		_putchar(num + 48);
 	}
 	_putchar('\n');
