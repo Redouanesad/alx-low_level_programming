aa849b7b87616d6b2cb9d7cd827aa8b9de73533b
Redouanesad <redouanesadouk7@gmail.com>
2023-06-22 12:47:11 -0700

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,11 +15,11 @@ void print_diagonal(int n)
 	{
 		int i, j;
 
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
-			for (j = 1; j <= postn; j++)
+			for (j = 0; j < n; j++)
 			{
-				if (i == j)
+				if (j == i)
 					_putchar('\\');
 				else if (j < i)
 					_putchar(' ');
