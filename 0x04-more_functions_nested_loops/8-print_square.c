67e2eabb158842ca711cb9e4b679ef7ef04fefd6
Redouanesad <redouanesadouk7@gmail.com>
2023-06-22 12:54:53 -0700

    a

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,17 @@
 
 void print_square(int size)
 {
-	int row, column;
-
-	for (row = 1; row <= size; row++)
-	{
-		for (column = 0; column < size; column++)
-			_putchar('#');
+	if (size <= 0)
 		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 0; i <= size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
 }
