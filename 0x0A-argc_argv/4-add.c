844b54225a613ea77f10f20521fa0612b203e499
Redouanesad <redouanesadouk7@gmail.com>
2023-07-07 18:21:48 +0100

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,6 +7,7 @@
  * @argv: array of pointers to arguments
  * Return: Always 0
 */
+
 int main(int argc, char *argv[])
 {
 	int sum = 0;
@@ -15,7 +16,7 @@ int main(int argc, char *argv[])
 	while (--argc)
 	{
 		for (c = argv[argc]; *c; c++)
-			if (*c < '0' || *c < '9')
+			if (*c < '0' || *c > '9')
 				return (printf("Error\n"), 1);
 		sum += atoi(argv[argc]);
 	}
